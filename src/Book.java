ef61ec7d24d976543673cbfac4e98382fb01a027
Javier Román <javier.romloz@gmail.com>
2026-08-31 18:08:21 +0200

    Add Book class with attributes, constructor, getters/setters and toString

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,49 @@
 public class Book {
+    private String title;
+    private String author;
+    private String publisher;
+    private BookStatus bookStatus;
+
+    //CONSTRUCTOR
+    public Book(String title, String author, String publisher, BookStatus bookStatus){
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.bookStatus = bookStatus;
+    }
+
+    //GETTERS & SETTERS
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+    public void setAuthor(String author){
+        this.author = author;
+    }
+
+    public String getPublisher(){
+        return publisher;
+    }
+    public void setPublisher(String publisher){
+        this.publisher = publisher;
+    }
+
+    public BookStatus getBookStatus(){
+        return bookStatus;
+    }
+    public void setBookStatus(BookStatus bookStatus){
+        this.bookStatus = bookStatus;
+    }
+
+    //toString METHOD
+    @Override
+    public String toString(){
+        return ("Título: " + title + " " +"Autor: " + author+ " " + "Editorial: " + publisher + " " + "Estado: " + bookStatus);
+    }
 }
